e0849c0315d88efc78dff948bb99a873d1abb56a
1221288 <1221288@isep.ipp.pt>
2024-10-20 21:08:16 +0100

    removed test folder

--- Backend/Testes/obj/Debug/net8.0/Testes.AssemblyInfo.cs
+++ Backend/Testes/obj/Debug/net8.0/Testes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4c3706e55ff781f5b40287fa901283684e8568")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acde4d33e7a794fbecd9d19de5ee752b30de9751")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
